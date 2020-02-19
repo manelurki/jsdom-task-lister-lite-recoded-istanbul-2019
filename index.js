b1293e7572e1a827258731c8a4d6b6f2ccbb23f6
manel turki <menelturki@gmail.com>
2020-02-19 20:24:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("submit").addEventListener("click",function(event){
      const a = document.getElementById("new-task-description");
       const a = document.getElementById("new-task-description");
+       const li = document.createElement("li");
      const b = document.getElementById("tasks");
     
   });
